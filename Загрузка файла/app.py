222894071f9e112d169365d07e5f621a6f011d7b
Real Keks <gdjvvjfisnkd@gmail.com>
2021-03-07 16:40:25 +0300

    Гы

--- Загрузка файла/app.py
+++ Загрузка файла/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, request, render_template
 import os
 
 app = Flask(__name__)
@@ -6,40 +6,12 @@ app = Flask(__name__)
 
 @app.route('/load_photo', methods=['POST', 'GET'])
 def empty():
-    if request.method == 'GET':
-        return f'''<!doctype html>
-                            <html lang="en">
-                              <head>
-                                <meta charset="utf-8">
-                                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                                <link rel="stylesheet"
-                                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                                crossorigin="anonymous">
-                                <link rel="stylesheet" type="text/css" href="{url_for("static", filename="css/form.css")}"/>
-                                <title>Пример формы</title>
-                              </head>
-                              <body>
-                                <h1 class=text-center>Загрузка фотографии</h1>
-                                <h2 class=text-center>Для участия в мисссии</h2>
-                                <div>
-                                    <form class="login_form" method="post" enctype="multipart/form-data">
-                                        <div class="form-group">
-                                            <label for="photo">Приложите фотографию</label>
-                                            <input type="file" class="form-control-file" id="photo" name="file">
-                                        </div>
-                                        {f'<img src="{url_for("static", filename="img/image.jpg")}">' * os.path.exists('static/img/image.jpg')}
-                                        <button type="submit" class="btn btn-primary">Отправить</button>
-                                    </form>
-                                </div>
-                              </body>
-                            </html>'''
-    elif request.method == 'POST':
-        if not os.path.exists('static/img'):
-            os.mkdir('static/img')
+    if not os.path.exists('static/img'):
+        os.mkdir('static/img')
+    if request.method == 'POST':
         with open('static/img/image.jpg', 'wb') as file:
             file.write(request.files['file'].read())
-        return ''
+    return render_template('login.html', flag=os.path.exists('static/img/image.jpg'))
 
 
 if __name__ == '__main__':
